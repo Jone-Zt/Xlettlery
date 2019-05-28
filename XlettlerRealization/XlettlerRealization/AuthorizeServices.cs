ef113cbecd34320b07496649187bd5768f9e0fc0
Tengxiaodai <1600550505@qq.com>
2019-05-28 18:53:11 +0800

    20190528001

---
+++ XlettlerRealization/XlettlerRealization/AuthorizeServices.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using ServicesInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlettlerRealization
+{
+    public class AuthorizeServices : IAuthorizeInterface
+    {
+        public bool CheckAuthorzeServices(string UserName, string UserPwd)
+        {
+            using (ModelContainer db = new ModelContainer()) 
+            {
+                SESENT_USERS uSERS= db.SESENT_USERS.Where(a => a.AccountID == UserName || a.Phone == UserName).FirstOrDefault();
+                if (uSERS == null)
+                    return false;
+                return uSERS.userPwd == UserPwd;
+            }
+        }
+    }
+}
